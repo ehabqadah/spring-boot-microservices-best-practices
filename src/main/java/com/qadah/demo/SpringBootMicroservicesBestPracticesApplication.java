fa969d5e8a5e233fee10204d1403f895442cec89
ehab <ehab@freightos.com>
2020-07-19 12:33:09 +0300

    Add OpenAPI docs configs

--- src/main/java/com/qadah/demo/SpringBootMicroservicesBestPracticesApplication.java
+++ src/main/java/com/qadah/demo/SpringBootMicroservicesBestPracticesApplication.java
@@ -1,7 +1,13 @@
 package com.qadah.demo;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Ehab Qadah
@@ -12,5 +18,6 @@ public class SpringBootMicroservicesBestPracticesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroservicesBestPracticesApplication.class, args);
 	}
+
 
 }
